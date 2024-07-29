4d7607317cda1620369e5419c01c55d8c79035fc
Jakov Knezevic <knezevic.jakov@gmail.com>
2024-07-29 14:30:38 +0200

    shadcn-ui added as a component library.

---
+++ src/app/_lib/shadcn-ui/shadcn.ts
@@ -0,0 +1,9 @@
+// utils
+import { clsx } from "clsx"
+import { twMerge } from "tailwind-merge"
+// types
+import type { ClassValue } from "clsx"
+
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs))
+}
