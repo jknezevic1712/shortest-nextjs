275b05ca1a4ea01d3ff880725adb8ca54333fa29
Jakov Knezevic <knezevic.jakov@gmail.com>
2024-07-29 16:41:19 +0200

    Added route guard that checks if user exists before proceeding to the wanted route. If user doesn't exist, route user to /authentication view.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,9 @@
-import type { Metadata } from 'next';
-import { Inter } from 'next/font/google';
 // utils
+import { Inter } from 'next/font/google';
 import '@app/_lib/styles/globals.css';
 import { Toaster } from '@/app/_utils/providers/Toaster';
+// types
+import type { Metadata } from 'next';
 
 const inter = Inter({ subsets: ['latin'] });
 
---
+++ src/app/middleware.ts
@@ -0,0 +1,30 @@
+// utils
+import { NextResponse } from 'next/server';
+// types
+import type { NextRequest } from 'next/server';
+
+export function middleware(request: NextRequest) {
+	const user = true;
+
+	if (!user) {
+		return NextResponse.redirect(new URL('/authentication', request.url));
+	}
+
+	return NextResponse.next();
+}
+
+// Apply middleware to specific paths
+export const config = {
+	matcher: [
+		/*
+		 * Match all request paths except for the ones starting with:
+		 * - api (API routes)
+		 * - _next/static (static files)
+		 * - _next/image (image optimization files)
+		 * - favicon.ico (favicon file)
+		 * Example:
+		 * '/((?!api|_next/static|_next/image|favicon.ico).*)',
+		 */
+		'/((?!authentication.*)',
+	],
+};
