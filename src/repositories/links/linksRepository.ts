ee785eafb052efa56d2cfa13886bf59847ed4743
Jakov Knezevic <knezevic.jakov@gmail.com>
2024-08-01 20:11:49 +0200

    Added creating, editing, and deleting links.

--- src/repositories/links/linksRepository.ts
+++ src/repositories/links/linksRepository.ts
@@ -6,6 +6,8 @@ import LinkDTO from '@/dtos/link';
 import ILinksRepository from '.';
 import type { SupabaseClient } from '@supabase/supabase-js';
 
+// TODO: If testing environment, use generateTestLinks function instead to fetch links and operate on them
+// TODO: If error happens, throw your own error
 export default class LinksRepository implements ILinksRepository {
   private _db: SupabaseClient;
 
@@ -18,20 +20,37 @@ export default class LinksRepository implements ILinksRepository {
   }
 
   public async fetchLinks() {
-    // TODO: If testing environment, use generateTestLinks function instead
-    // TODO: If error happens, throw your own error
     const { data, error } = await this._db.from('links').select();
 
     return data as LinkDTO[];
   }
 
   public async createLink(link: LinkDTO) {
-    const linkCollection = this.fetchLinks();
-    return linkCollection;
+    const { data, error } = await this._db
+      .from('links')
+      .insert(link.linkData)
+      .select();
+
+    return data as LinkDTO[];
+  }
+
+  public async editLink(link: LinkDTO) {
+    const { data, error } = await this._db
+      .from('links')
+      .update(link.linkData)
+      .eq('id', link.linkData.id)
+      .select();
+
+    return data as LinkDTO[];
   }
 
   public async deleteLink(linkId: string) {
-    const linkCollection = this.fetchLinks();
-    return linkCollection;
+    const { data, error } = await this._db
+      .from('links')
+      .delete()
+      .eq('id', linkId)
+      .select();
+
+    return data as LinkDTO[];
   }
 }
