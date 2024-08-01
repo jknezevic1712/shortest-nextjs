68051c67a07386f2aa104966de64c7f04a60f1e4
Jakov Knezevic <knezevic.jakov@gmail.com>
2024-08-01 20:12:33 +0200

    Update to ILinksRepository.

--- src/repositories/links/index.ts
+++ src/repositories/links/index.ts
@@ -1,7 +1,8 @@
 import LinkDTO from '@/dtos/link';
 
 export default interface ILinksRepository {
-	fetchLinks(): Promise<LinkDTO[]>;
-	createLink(link: LinkDTO): Promise<LinkDTO[]>;
-	deleteLink(linkId: string): Promise<LinkDTO[]>;
+  fetchLinks(): Promise<LinkDTO[]>;
+  createLink(link: LinkDTO): Promise<LinkDTO[]>;
+  editLink(link: LinkDTO): Promise<LinkDTO[]>;
+  deleteLink(linkId: string): Promise<LinkDTO[]>;
 }
