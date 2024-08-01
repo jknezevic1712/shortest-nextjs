e573685f471abf8cd119292b36bdd2d4f95a7f7e
Jakov Knezevic <knezevic.jakov@gmail.com>
2024-08-01 19:57:47 +0200

    Update to linkDTO.

--- src/dtos/link.ts
+++ src/dtos/link.ts
@@ -1,37 +1,38 @@
-import { Link } from '@/app/_lib/types/links';
+// types
+import type { Link } from '@/app/_lib/types/links';
 
 export default class LinkDTO {
-	private _id: string;
-	private _original: string;
-	private _shortened: string | undefined;
+  private _id: string;
+  private _original: string;
+  private _shortened: string | undefined;
 
-	constructor({
-		id,
-		original,
-		shortened,
-	}: {
-		id: string;
-		original: string;
-		shortened?: string;
-	}) {
-		this._id = id;
-		this._original = original;
-		this._shortened = shortened;
-	}
+  constructor({
+    id,
+    original,
+    shortened,
+  }: {
+    id: string;
+    original: string;
+    shortened?: string;
+  }) {
+    this._id = id;
+    this._original = original;
+    this._shortened = shortened;
+  }
 
-	public get linkData() {
-		return {
-			id: this._id,
-			original: this._original,
-			new: this._shortened,
-		};
-	}
+  public get linkData() {
+    return {
+      id: this._id,
+      original: this._original,
+      new: this._shortened,
+    };
+  }
 
-	static fromDb(data: Link) {
-		return new LinkDTO({
-			id: data.id,
-			original: data.original,
-			shortened: data.shortened,
-		});
-	}
+  static fromDb(data: Link) {
+    return new LinkDTO({
+      id: data.id,
+      original: data.original,
+      shortened: data.shortened,
+    });
+  }
 }
