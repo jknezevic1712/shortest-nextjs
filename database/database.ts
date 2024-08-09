4ed51380a92373c364d1524be85dbd1b5165e573
Jakov Knezevic <knezevic.jakov@gmail.com>
2024-08-09 14:01:02 +0200

    Database types update.

--- database/database.ts
+++ database/database.ts
@@ -3,7 +3,7 @@ import { createClient } from '@supabase/supabase-js';
 import type { SupabaseClient } from '@supabase/supabase-js';
 import type { Database } from './database.type';
 
-export type DatabaseClient = SupabaseClient;
+export type DatabaseClient = SupabaseClient<Database>;
 
 export const databaseClient = createClient<Database>(
 	process.env.NEXT_PUBLIC_SUPABASE_URL!,
