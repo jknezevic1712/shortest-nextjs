70f4b1283e0fcadf93fc550fad062cd5b22eaefc
Jakov Knezevic <knezevic.jakov@gmail.com>
2024-08-21 17:46:42 +0200

    Extended useServerActions hook to include new auth actions.

--- src/app/_lib/hooks/useServerActions.ts
+++ src/app/_lib/hooks/useServerActions.ts
@@ -1,14 +1,26 @@
 'use client';
 
 import { useServerAction } from 'zsa-react';
-import { createLink, editLink } from '@/app/dashboard/actions';
+import {
+	createLink,
+	editLink,
+	getUser,
+	loginUserWithProvider,
+	logoutUser,
+} from '@/app/dashboard/actions';
 
 export default function useServerActions() {
 	const createLinkAction = useServerAction(createLink);
 	const editLinkAction = useServerAction(editLink);
+	const signInWithProvider = useServerAction(loginUserWithProvider);
+	const signOutUser = useServerAction(logoutUser);
+	const getUserData = useServerAction(getUser);
 
 	return {
 		createLinkAction,
 		editLinkAction,
+		signInWithProvider,
+		signOutUser,
+		getUserData,
 	};
 }
